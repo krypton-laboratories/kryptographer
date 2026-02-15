611f0d2078e0fe0e1f3f913e9407c593602bdbad
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2026-02-15 00:27:15 -0600

    Animated home page

--- script.js
+++ script.js
@@ -8,22 +8,21 @@ document.querySelector('header').innerHTML = `
 `;
 
 // Fade-in animations
-if (document.querySelector('body').classList.contains('about')) {
-    const observerOptions = {
-        root: null, // Watches entire viewport
-        threshold: 0.1 // Trigger: element 10% visible
-    };
-
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-                observer.unobserver(entry.target); // Stops watching after element appears
-            }
-        });
-    }, observerOptions);
-
-    document.querySelectorAll('.fade-in').forEach(card => {
-        observer.observe(card);
+const observerOptions = {
+    root: null, // Watches entire viewport
+    threshold: 0.1 // Trigger: element 10% visible
+};
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserver(entry.target); // Stops watching after element appears
+        }
+    });
+}, observerOptions);
+const fadeItems = document.querySelectorAll('.fade-in');
+if (fadeItems.length > 0) {
+    fadeItems.forEach(item => {
+        observer.observe(item);
     });
 }
